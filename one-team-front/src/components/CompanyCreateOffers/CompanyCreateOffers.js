9ae5bd23f60d6c64dec0bdbc9f3cbba7ecc4f958
David Huveau <d.huveau@gmail.com>
2018-12-04 15:20:07 +0100

    comonent controled without axios

--- one-team-front/src/components/CompanyCreateOffers/CompanyCreateOffers.js
+++ one-team-front/src/components/CompanyCreateOffers/CompanyCreateOffers.js
@@ -1,16 +1,80 @@
 import React from "react";
+import "./CompanyCreateOffers.css";
 
 const CompanyCreateOffers = class extends React.Component {
+  state = {
+    title: "",
+    startDate: new Date().toLocaleDateString(),
+    endDate: new Date().toLocaleDateString(),
+    descritpion: "",
+    town: "",
+    intro: ""
+  };
+
+  handlerOnChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handlerOnSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
+    const { title, startDate, endDate, descritpion, town, intro } = this.state;
     return (
-      <div>
+      <div className="CompanyCreateOffers">
         <p>Je crée une mission</p>
-        <form>
-          <input placeholder="Titre de la mission de stage" />
-          <input placeholder="Date de début" />
-          <input placeholder="Date de fin" />
-          <text placeholder="Description" />
-          <button type="button">Valider</button>
+        <form
+          method="post"
+          onSubmit={this.handlerOnSubmit}
+          className="container"
+        >
+          <input
+            placeholder="Titre de la mission de stage"
+            name="title"
+            value={title}
+            onChange={this.handlerOnChange}
+            required
+          />
+          <input
+            placeholder="Date de début"
+            name="startDate"
+            value={startDate}
+            onChange={this.handlerOnChange}
+            required
+          />
+          <input
+            placeholder="Date de fin"
+            name="endDate"
+            value={endDate}
+            onChange={this.handlerOnChange}
+            required
+          />
+          <input
+            placeholder="Ville"
+            name="town"
+            value={town}
+            onChange={this.handlerOnChange}
+            required
+          />
+          <textarea
+            placeholder="Introduction"
+            name="intro"
+            value={intro}
+            onChange={this.handlerOnChange}
+            required
+          />
+          <textarea
+            placeholder="Description"
+            name="descritpion"
+            value={descritpion}
+            onChange={this.handlerOnChange}
+            required
+          />
+          <input type="submit" className="submit" />
         </form>
         <p>cette offre sera publiée après validation</p>
       </div>
