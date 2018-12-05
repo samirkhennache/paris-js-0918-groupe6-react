749783f451743922be90f1911e96f3815ed53eee
Camille <camille.coutens@gmail.com>
2018-12-05 11:20:19 +0100

    app sauvegardé

--- one-team-front/src/App.js
+++ one-team-front/src/App.js
@@ -1,34 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
-<<<<<<< HEAD
-import ConnexionTrainee from "./components/authentication/ConnexionTrainee";
-import StudentCreateAccount from "./components/authentication/StudentCreateAccount";
-import CompanyOffers from "./components/CompanyOffers/CompanyOffers";
-=======
 // import StudentCreateAccount from "./components/StudentCreateAccount";
 import CompanyOffers from "./components/CompanyOffers/CompanyOffers";
 import ConnexionTrainee from "./components/authentication/ConnexionTrainee";
 import StudentCreateAccount from "./components/authentication/StudentCreateAccount";
 import ConnexionCompany from "./components/authentication/ConnexionCompany";
 import CompanyCreateAccount from "./components/authentication/CompanyCreateAccount";
->>>>>>> bc462ca47c12d0cda1c06298484fe16efdaed200
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CompanyOffers />
-<<<<<<< HEAD
         {/* <StudentCreateAccount />
         <ConnexionTrainee /> */}
-=======
         STUDENT
         <StudentCreateAccount />
         <ConnexionTrainee />
         COMPANY
         <CompanyCreateAccount />
         <ConnexionCompany />
->>>>>>> bc462ca47c12d0cda1c06298484fe16efdaed200
       </div>
     );
   }
