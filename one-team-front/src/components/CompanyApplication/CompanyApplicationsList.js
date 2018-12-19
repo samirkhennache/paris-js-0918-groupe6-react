145c020241009943efbfa2aaa1bbd7f593cc07aa
MohamedWild <kerkeb.moha@gmail.com>
2018-12-19 15:41:43 +0100

    mise a jour

---
+++ one-team-front/src/components/CompanyApplication/CompanyApplicationsList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CompanyApplicationsList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>CompanyApplicationsList</h1>
+      </div>
+    );
+  }
+}
+
+export default CompanyApplicationsList;
