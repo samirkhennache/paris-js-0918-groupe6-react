b4104de0db472d99b8c566f7ea7033545bd7c154
Camille <camille.coutens@gmail.com>
2019-01-07 17:55:24 +0100

    mise en place du routeur pour la partie admin

---
+++ one-team-front/src/components/admin/OfferCompletedItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { OfferView } from "../offerView";
+import CompanyOffers from "../companyOffers/CompanyOffers";
+
+export default class OfferCompletedItem extends Component {
+  render() {
+    return (
+      <div>
+        <CompanyOffers />
+      </div>
+    );
+  }
+}
---
+++ one-team-front/src/components/admin/OffersCompletedList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import OfferCompletedItem from "./OfferCompletedItem";
+
+export default class OffersCompletedList extends Component {
+  render() {
+    return (
+      <div>
+        <OfferCompletedItem />
+      </div>
+    );
+  }
+}
