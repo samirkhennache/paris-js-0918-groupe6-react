ac7e7799db52ab6e21e69b9c2ab4ec3d2c1c661b
Camille <camille.coutens@gmail.com>
2019-01-17 11:53:39 +0100

    changement de formulaire via lien (par ternaire) ok

--- one-team-front/src/components/authentication/companyCreateConnexion/CompanyCreateConnexion.js
+++ one-team-front/src/components/authentication/companyCreateConnexion/CompanyCreateConnexion.js
@@ -1,14 +1,50 @@
 import React, { Component } from "react";
 import CompanyCreateAccount from "./CompanyCreateAccount";
 import ConnexionCompany from "./ConnexionCompany";
+import { Link } from "react-router-dom";
+
+import Paper from "@material-ui/core/Paper";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 class CompanyCreateConnexion extends Component {
+  state = {
+    connexion: true
+  };
+
+  clickAccount = () => {
+    this.setState({ connexion: false });
+  };
+
+  clickConnexion = () => {
+    this.setState({ connexion: true });
+  };
   render() {
+    const { connexion } = this.state;
     return (
       <div>
         <h3>Page création connection Company</h3>
-        <CompanyCreateAccount {...this.props} />
-        <ConnexionCompany {...this.props} />
+        {connexion ? (
+          <ConnexionCompany {...this.props} />
+        ) : (
+          <CompanyCreateAccount {...this.props} />
+        )}
+        {connexion ? (
+          <p>
+            Vous n'êtes pas encore sur OneTeam ?{" "}
+            <a href="#" onClick={this.clickAccount}>
+              Inscrivez-vous !
+            </a>{" "}
+          </p>
+        ) : (
+          <p>
+            J'ai déjà un compte,{" "}
+            <a href="#" onClick={this.clickConnexion}>
+              je me connecte
+            </a>{" "}
+            !
+          </p>
+        )}
       </div>
     );
   }
