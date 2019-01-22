bbefba3e06fc43a6b15b5ee6b64bfc730eadcd96
Prudence Gaboriau <prudence.gaboriau@gmail.com>
2019-01-22 10:39:19 +0100

    hr modifs

--- one-team-front/src/components/traineeApplications/TraineeApplications.js
+++ one-team-front/src/components/traineeApplications/TraineeApplications.js
@@ -9,6 +9,8 @@ class TraineeApplications extends Component {
     return (
       <div>
         <div className="bandeau-candidatures-trainee">
+        <h1>Candidatures</h1>
+        <h2>Tu peux consulter et suivre l'avancement de tes candidatures</h2>
           <img src={bandeauTrainee} alt="candidatures" />
         </div>
         <StudentApplicationList {...this.props} />
