d3876575aeec6ffb0acc34d6671c7d182e113fb9
Chopin85 <p.cata85@gmail.com>
2019-01-23 15:53:43 +0100

    Finish page Comment ça marche

--- one-team-front/src/components/home/traineePart/cursus.js
+++ one-team-front/src/components/home/traineePart/cursus.js
@@ -1,30 +1,53 @@
 import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
-import "./cursus.css";
-import blackNumber1 from "../../../img/students/black-1.png";
-import blackNumber2 from "../../../img/students/black-2.png";
-import blackNumber3 from "../../../img/students/black-3.png";
-import blackNumber4 from "../../../img/students/black-4.png";
-import blackNumber5 from "../../../img/students/black-5.png";
+
+import "./Cursus.css";
+import homejob from "../../../img/icons/home-job.png";
+import homemeeting from "../../../img/icons/home-meeting.png";
+import homenotebook from "../../../img/icons/home-notebook.png";
+import homeuser from "../../../img/icons/home-user.png";
+
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import TraineeCreateConnexion from "../../authentication/studentCreateConnexion/TraineeCreateConnexion";
 
 class Cursus extends Component {
+  state = {
+    openTrainee: false,
+    button: "fermer"
+  };
+
+  traineeOpenConnexion = () => {
+    this.setState({
+      openTrainee: true
+    });
+  };
+
+  handleCloseTrainee = () => {
+    this.setState({ openTrainee: false });
+  };
+
   render() {
+    const { openTrainee, button } = this.state;
     return (
       <div className="rules-bloc general_margin">
         <Grid container justify="center">
           <Grid item xs={12}>
-            <h2 className="home_subsection_orange">Sur le plan pratique</h2>
+            <h2 className="home_subsection_orange">Comment ça marche ?</h2>
+            <hr className="hr_horizontal_orange_home" />
           </Grid>
           {/* *************** RULES 1 ********************* */}
           <Grid item xs={12} sm={6} md={3} lg={3}>
             <div className="rules-item">
               <div>
-                <img src={blackNumber1} alt="1" />
+                <img src={homeuser} className="cursusImage" alt="1" />
               </div>
-              <p className="icon_orange_title">Complète ton profil</p>
+              <p className="icon_orange_title">Publie ton profil</p>
               <p>
-                Après avoir créer ton compte, remplis soigneusement ta fiche
-                publique afin d’être visible auprès des recruteurs
+                Après avoir créer ton compte, remplis ta fiche publique afin
+                d’être visible auprès des recruteurs
               </p>
             </div>
           </Grid>
@@ -32,13 +55,14 @@ class Cursus extends Component {
           <Grid item xs={12} sm={6} md={3} lg={3}>
             <div className="rules-item">
               <div>
-                <img src={blackNumber2} alt="2" />
+                <img src={homejob} className="cursusImage" alt="2" />
               </div>
-              <p className="icon_orange_title">Pas de CV ni de lettre de motivation standards</p>
+              <p className="icon_orange_title">Pas de CV standardisé</p>
               <p>
-                Parle-nous de ton cursus, des tes compétences spécifiques.
-                Dis-nous ce qui te motive, ce que tu souhaite apporter à une
-                équipe
+                Ni CV, ni lettre de motivation, parle nous de ton cursus
+                d’études et de tes compétences personnelles et techniques. Dis
+                nous ce qui te motive, ce en quoi tu crois et ce que tu veux
+                apporter à une équipe.
               </p>
             </div>
           </Grid>
@@ -46,12 +70,12 @@ class Cursus extends Component {
           <Grid item xs={12} sm={6} md={3} lg={3}>
             <div className="rules-item">
               <div>
-                <img src={blackNumber3} alt="3" />
+                <img src={homenotebook} className="cursusImage" alt="3" />
               </div>
-              <p className="icon_orange_title">1,2,3 postule !</p>
+              <p className="icon_orange_title">1, 2, 3 postule !</p>
               <p>
-                Lance toi, envoie tes candidatures auprès des entreprises et des
-                missions qui te plaisent.
+                Candidate auprès des entreprises et des missions qui te
+                plaisent.
               </p>
             </div>
           </Grid>
@@ -59,30 +83,40 @@ class Cursus extends Component {
           <Grid item xs={12} sm={6} md={3} lg={3}>
             <div className="rules-item">
               <div>
-                <img src={blackNumber4} alt="4" />
+                <img src={homemeeting} className="cursusImage" alt="4" />
               </div>
-              <p className="icon_orange_title">Prépare ton entretien</p>
+              <p className="icon_orange_title">Prépare ton entretien</p>
               <p>
-                Lorsque qu’une entreprise à sélectionné sa team, un consultant
-                de One Team prépare les membres de l’équipe au premier entretien
-              </p>
-            </div>
-          </Grid>
-          {/* *************** RULES 5 ********************* */}
-          <Grid item xs={12} sm={6} md={3} lg={3}>
-            <div className="rules-item">
-              <div>
-                <img src={blackNumber5} alt="5" />
-              </div>
-              <p className="icon_orange_title">Bravo ta team a été recrutée</p>
-              <p>
-                Pendant ton stage One Team peut réaliser à la demande de
-                l’entreprise un coaching d’équipe ponctué par des points
-                d’avancement réguliers
+                Lorsqu’une entreprise ta sélectioner au sein de sa team, un
+                consultant OneTeam prépare les membres de l’équipe au premier
+                entretien
               </p>
             </div>
           </Grid>
         </Grid>
+        <Button
+          variant="contained"
+          className="buttonTrainee"
+          onClick={this.traineeOpenConnexion}
+        >
+          Créer son compte
+        </Button>
+        {/* DIALOG TRAINEE ---------------------------------------- */}
+        <Dialog
+          open={openTrainee}
+          onClose={this.handleCloseTrainee}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogContent>
+            <TraineeCreateConnexion {...this.props} />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleCloseTrainee} color="primary">
+              {button}
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }
