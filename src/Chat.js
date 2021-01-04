728506521c98df3612771e2a5e17f5f79785a457
samir.khennache <samir.khennache@rakuten.com>
2021-01-04 15:15:48 +0100

    sendBird event handlers

--- src/Chat.js
+++ src/Chat.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/prop-types */
 import React, { useState } from "react";
+import * as SendBird from "sendbird";
 
 import "./Chat.css";
 
@@ -44,6 +45,33 @@ export default function Chat() {
   //     </button>
   //   </div>
   // );
+  const queries = {
+    applicationUserListQuery: {
+      limit: 0
+    }
+  };
+
+  const sb = new SendBird({ appId: "D36ADF40-475A-46DF-98B5-38BD1182C989" });
+  const channelHandler = new sb.ChannelHandler();
+  const channelListQuery = sb.GroupChannel.createMyGroupChannelListQuery();
+  channelListQuery.includeEmpty = false;
+  channelListQuery.order = "latest_last_message"; // 'chronological', 'latest_last_message', 'channel_name_alphabetical', and 'metadata_value_alphabetical'
+  channelListQuery.limit = 15; // The value of pagination limit could be set up to 100.
+
+  if (channelListQuery.hasNext) {
+    channelListQuery.next((groupChannels, error) => {
+      if (error) {
+        return;
+      }
+
+      console.log(groupChannels);
+    });
+  }
+  channelHandler.onMessageReceived = function(channel, message) {
+    console.log("messad", message);
+  };
+
+  sb.addChannelHandler(sessionStorage.getItem("access_token"), channelHandler);
   return (
     <div style={{ height: "100vh", marginBottom: "64px" }}>
       <SendBirdProvider
@@ -52,12 +80,12 @@ export default function Chat() {
         userId={dataUser.id.toString()}
         nickname={dataUser.firstname}
         allowProfileEdit={false}
+        disableUserProfile
       >
-        {/* <WelcomeWithSendBird /> */}
-        {/* <CustomComponentWithSendBird /> */}
         <div className="sendbird-app__wrap">
           <div className="sendbird-app__channellist-wrap">
             <ChannelList
+              queries={queries}
               isOnline
               allowProfileEdit={false}
               onChannelSelect={channel => {
@@ -66,18 +94,6 @@ export default function Chat() {
                 }
               }}
             />
-            {/* <div style={{ height: "520px" }}>
-              <ChannelList
-                useMessageGrouping={false}
-                renderChannelPreview={MyCustomPreview}
-                onChannelSelect={channel => {
-                  if (channel && channel.url) {
-                    setCurrentChannelUrl(channel.url);
-                  }
-                  console.warn(channel);
-                }}
-              />
-            </div> */}
           </div>
           <div className="sendbird-app__conversation-wrap">
             <Channel
